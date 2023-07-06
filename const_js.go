644eba0c41cbc042af8e24a6ac15a96ef1800130
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-07-06 15:41:20 +0530

    fix build tags

---
+++ const_js.go
@@ -0,0 +1,8 @@
+//go:build js
+
+package metrics
+
+const (
+	// DefaultSignal is used with DefaultInmemSignal
+	DefaultSignal = 0
+)
--- const_unix.go
+++ const_unix.go
@@ -1,4 +1,4 @@
-// +build !windows
+//go:build unix && !js
 
 package metrics
 
